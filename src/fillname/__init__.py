1d32dd3872515deb169b7662912934daa1cd757f
Amade Nemes <nemesamade@gmail.com>
2023-05-10 15:05:25 +0200

    Fix entry point.
    setup.cfg specifies the entry point console_scrips=fillname:main, but
    the main function was not importable. Moving to main __init__.py fixes
    this, and allows us to get rid of the separate __main__.py file.

--- src/fillname/__init__.py
+++ src/fillname/__init__.py
@@ -1,3 +1,23 @@
 """
 The fillname project.
 """
+from .utils.logger import setup_logger
+from .utils.parser import get_parser
+
+
+def main():
+    """
+    Run the main function.
+    """
+    parser = get_parser()
+    args = parser.parse_args()
+    log = setup_logger("main", args.log)
+
+    log.info("info")
+    log.warning("warning")
+    log.debug("debug")
+    log.error("error")
+
+
+if __name__ == "__main__":
+    main()
